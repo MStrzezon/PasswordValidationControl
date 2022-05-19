db8048dee2db7da82b8f05a643ecbbf5c8ee4007
Michal Strzezon <mstrzezon@gmail.com>
2022-05-19 22:19:44 +0200

    Add project files.

---
+++ PasswordValidator.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PasswordValidationControl
+{
+    partial class PasswordValidator
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.check4Term = new System.Windows.Forms.PictureBox();
+            this.check3Term = new System.Windows.Forms.PictureBox();
+            this.check2Term = new System.Windows.Forms.PictureBox();
+            this.check1Term = new System.Windows.Forms.PictureBox();
+            this.uncheck4Term = new System.Windows.Forms.PictureBox();
+            this.uncheck3Term = new System.Windows.Forms.PictureBox();
+            this.uncheck2Term = new System.Windows.Forms.PictureBox();
+            this.uncheck1Term = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.check4Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check3Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check2Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check1Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck4Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck3Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck2Term)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck1Term)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "at least 8 characters";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "at least one special character";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "at least one capital letter";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 73);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "at least one digit";
+            // 
+            // check4Term
+            // 
+            this.check4Term.Image = global::PasswordValidationControl.Properties.Resources.check;
+            this.check4Term.Location = new System.Drawing.Point(4, 72);
+            this.check4Term.Name = "check4Term";
+            this.check4Term.Size = new System.Drawing.Size(17, 17);
+            this.check4Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.check4Term.TabIndex = 11;
+            this.check4Term.TabStop = false;
+            this.check4Term.Visible = false;
+            // 
+            // check3Term
+            // 
+            this.check3Term.Image = global::PasswordValidationControl.Properties.Resources.check;
+            this.check3Term.Location = new System.Drawing.Point(4, 50);
+            this.check3Term.Name = "check3Term";
+            this.check3Term.Size = new System.Drawing.Size(17, 17);
+            this.check3Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.check3Term.TabIndex = 10;
+            this.check3Term.TabStop = false;
+            this.check3Term.Visible = false;
+            // 
+            // check2Term
+            // 
+            this.check2Term.Image = global::PasswordValidationControl.Properties.Resources.check;
+            this.check2Term.Location = new System.Drawing.Point(4, 27);
+            this.check2Term.Name = "check2Term";
+            this.check2Term.Size = new System.Drawing.Size(17, 17);
+            this.check2Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.check2Term.TabIndex = 9;
+            this.check2Term.TabStop = false;
+            this.check2Term.Visible = false;
+            // 
+            // check1Term
+            // 
+            this.check1Term.Image = global::PasswordValidationControl.Properties.Resources.check;
+            this.check1Term.Location = new System.Drawing.Point(4, 4);
+            this.check1Term.Name = "check1Term";
+            this.check1Term.Size = new System.Drawing.Size(17, 17);
+            this.check1Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.check1Term.TabIndex = 8;
+            this.check1Term.TabStop = false;
+            this.check1Term.Visible = false;
+            // 
+            // uncheck4Term
+            // 
+            this.uncheck4Term.Image = global::PasswordValidationControl.Properties.Resources.remove;
+            this.uncheck4Term.Location = new System.Drawing.Point(4, 72);
+            this.uncheck4Term.Name = "uncheck4Term";
+            this.uncheck4Term.Size = new System.Drawing.Size(17, 17);
+            this.uncheck4Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.uncheck4Term.TabIndex = 3;
+            this.uncheck4Term.TabStop = false;
+            // 
+            // uncheck3Term
+            // 
+            this.uncheck3Term.Image = global::PasswordValidationControl.Properties.Resources.remove;
+            this.uncheck3Term.Location = new System.Drawing.Point(4, 49);
+            this.uncheck3Term.Name = "uncheck3Term";
+            this.uncheck3Term.Size = new System.Drawing.Size(17, 17);
+            this.uncheck3Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.uncheck3Term.TabIndex = 2;
+            this.uncheck3Term.TabStop = false;
+            // 
+            // uncheck2Term
+            // 
+            this.uncheck2Term.Image = global::PasswordValidationControl.Properties.Resources.remove;
+            this.uncheck2Term.Location = new System.Drawing.Point(4, 27);
+            this.uncheck2Term.Name = "uncheck2Term";
+            this.uncheck2Term.Size = new System.Drawing.Size(17, 17);
+            this.uncheck2Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.uncheck2Term.TabIndex = 1;
+            this.uncheck2Term.TabStop = false;
+            // 
+            // uncheck1Term
+            // 
+            this.uncheck1Term.Image = global::PasswordValidationControl.Properties.Resources.remove;
+            this.uncheck1Term.Location = new System.Drawing.Point(4, 4);
+            this.uncheck1Term.Name = "uncheck1Term";
+            this.uncheck1Term.Size = new System.Drawing.Size(17, 17);
+            this.uncheck1Term.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.uncheck1Term.TabIndex = 0;
+            this.uncheck1Term.TabStop = false;
+            // 
+            // PasswordValidator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.check4Term);
+            this.Controls.Add(this.check3Term);
+            this.Controls.Add(this.check2Term);
+            this.Controls.Add(this.check1Term);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uncheck4Term);
+            this.Controls.Add(this.uncheck3Term);
+            this.Controls.Add(this.uncheck2Term);
+            this.Controls.Add(this.uncheck1Term);
+            this.Name = "PasswordValidator";
+            this.Size = new System.Drawing.Size(218, 94);
+            ((System.ComponentModel.ISupportInitialize)(this.check4Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check3Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check2Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.check1Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck4Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck3Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck2Term)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uncheck1Term)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox uncheck1Term;
+        private System.Windows.Forms.PictureBox uncheck2Term;
+        private System.Windows.Forms.PictureBox uncheck3Term;
+        private System.Windows.Forms.PictureBox uncheck4Term;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox check1Term;
+        private System.Windows.Forms.PictureBox check2Term;
+        private System.Windows.Forms.PictureBox check3Term;
+        private System.Windows.Forms.PictureBox check4Term;
+    }
+}
---
+++ PasswordValidator.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordValidationControl
+{
+    public partial class PasswordValidator : UserControl
+    {
+        public PasswordValidator()
+        {
+            InitializeComponent();
+            MinimumNumberOfCharacters = 8;
+            specialCharacters = new List<char> { '$', '#', '@', '!', '%' };
+        }
+
+        public event EventHandler IsMinimumNumberOfCharactersChanged;
+        public event EventHandler IsSpecialCharacterChanged;
+        public event EventHandler IsCapitalLetterChanged;
+        public event EventHandler IsDigitChanged;
+        public event EventHandler ValidPasswordChanged;
+
+        private int minimumNumberOfCharacters;
+        private List<char> specialCharacters = new List<char>();
+
+        private bool isMinimumNumberOfCharacters;
+        private bool isSpecialCharacter;
+        private bool isCapitalLetter;
+        private bool isDigit;
+
+        private string password;
+
+        public int MinimumNumberOfCharacters
+        {
+            get { return minimumNumberOfCharacters; }
+            set { minimumNumberOfCharacters = value; label1.Text = "at least " + MinimumNumberOfCharacters + " characters"; }
+        }
+
+        public List<char> SpecialCharacters
+        {
+            get { return specialCharacters; }
+            set { specialCharacters = value; }
+        }
+
+        public bool IsMinimumNumberOfCharacters { get => isMinimumNumberOfCharacters; }
+        public bool IsSpecialCharacter { get => isSpecialCharacter; }
+        public bool IsCapitalLetter { get => isCapitalLetter; }
+        public bool IsDigit { get => isDigit; }
+
+        public string Password { set
+            {
+                password = value;
+                bool tmpValid = Valid;
+                bool tmpIsDigit = false;
+                bool tmpIsSpecialCharacter = false;
+                bool tmpIsCapitalLetter = false;
+
+                if (password.Length >= MinimumNumberOfCharacters)
+                {
+                    if (isMinimumNumberOfCharacters == false)
+                    {
+                        isMinimumNumberOfCharacters = true;
+                        IsMinimumNumberOfCharactersChanged(this, null);
+                    }
+                } else
+                {
+                    if (isMinimumNumberOfCharacters == true)
+                    {
+                        isMinimumNumberOfCharacters = false;
+                        IsMinimumNumberOfCharactersChanged(this, null);
+                    }
+                }
+
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (Char.IsDigit(password[i]))
+                    {
+                        tmpIsDigit = true;
+                    } 
+                    if (Char.IsUpper(password[i]))
+                    {
+                        tmpIsCapitalLetter = true; 
+                    }
+                    if (SpecialCharacters.Contains(password[i]))
+                    {
+                        tmpIsSpecialCharacter = true; 
+                    }
+                }
+
+                if (isDigit != tmpIsDigit)
+                {
+                    isDigit = tmpIsDigit;
+                    IsDigitChanged(this, null);
+                }
+                if (isSpecialCharacter != tmpIsSpecialCharacter)
+                {
+                    isSpecialCharacter = tmpIsSpecialCharacter;
+                    IsSpecialCharacterChanged(this, null);
+                }
+                if (isCapitalLetter != tmpIsCapitalLetter)
+                {
+                    isCapitalLetter = tmpIsCapitalLetter;
+                    IsCapitalLetterChanged(this, null);
+                }
+
+                if (Valid != tmpValid)
+                {
+                    ValidPasswordChanged(this, null);
+                }
+            } }
+
+        public bool Valid
+        {
+            get { return isMinimumNumberOfCharacters && isSpecialCharacter && isCapitalLetter && isDigit; }
+        }
+
+        public void uncheckMinimumNumberOfCharacters()
+        {
+            uncheck1Term.Show();
+            check1Term.Hide();
+        }
+
+        public void checkMinimumNumberOfCharacters()
+        {
+            uncheck1Term.Hide();
+            check1Term.Show();
+        }
+
+        public void uncheckSpecialCharacter()
+        {
+            uncheck2Term.Show();
+            check2Term.Hide();
+        }
+
+        public void checkSpecialCharacter()
+        {
+            uncheck2Term.Hide();
+            check2Term.Show();
+        }
+
+        public void uncheckCapitalLetter()
+        {
+            uncheck3Term.Show();
+            check3Term.Hide();
+        }
+
+        public void checkCapitalLetter()
+        {
+            uncheck3Term.Hide();
+            check3Term.Show();
+        }
+
+        public void uncheckDigit()
+        {
+            uncheck4Term.Show();
+            check4Term.Hide();
+        }
+
+        public void checkDigit()
+        {
+            uncheck4Term.Hide();
+            check4Term.Show();
+        }
+    }
+}
